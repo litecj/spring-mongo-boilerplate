db5ce79bc8cfafd3aaa9f4db290ecea5baccb639
litecj <litecj702@gmail.com>
2021-08-11 11:41:07 +0900

    first commit

---
+++ src/test/java/shop/tripn/app/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.tripn.app.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
